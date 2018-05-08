b0092da4f3ddc962bf2ec23d20c73f9de17882b9
lesnat <esnault.leo@orange.fr>
2018-05-08 18:53:10 +0200

    Use numpy method to get configurations (not working yet)

--- p2sat/_Hist.py
+++ p2sat/_Hist.py
@@ -8,11 +8,126 @@ class _Hist(object):
   def __init__(self,PhaseSpace):
     self._ps=PhaseSpace
     r=self._ps.raw
-    
+
+  # def hn(self,axis,blen=None,bwidth=None,brange=None,wnorm=None,select=None):
+  #   """
+  #   Create and return the n-dimensional histo of axis list.
+  #
+  #   Parameters
+  #   ----------
+  #   axis : list of str/np.array
+  #     list of axis to hist
+  #   blen : list of int, optional
+  #     list of number of bins. If a blen element is None, the default value is 10
+  #   bwidth : list of float, optional
+  #     list of bin width. If a bwidth element is None, a calculation is done to have 10 bins in the correspondant axis
+  #   brange : list of list of 2 float, optional
+  #     list of bin minimum and maximum. If a brange element is None, the minimum/maximum of the axis is taken
+  #   wnorm : list of float, optional
+  #     weight normalization. If a wnorm element is None, the bin width is taken
+  #   select : dict, optional
+  #     filtering dictionary
+  #
+  #   Returns
+  #   -------
+  #   b : np.array
+  #     bins
+  #   h : np.array
+  #     number of particles per bin unit
+  #
+  #   Notes
+  #   -----
+  #   TODO: If the given maximum bin range does not match with an int number of bins, the last bin is oversized ??
+  #   it reduce bwidth to fit brange[1]-brange[0] with a int nb of bins
+  #
+  #   If blen and bwidth are both defined, priority is given to blen.
+  #
+  #   Examples
+  #   --------
+  #
+  #   >>> hn(['x'],bwidth=[50],brange=[[0,1000]],wnorm=[1.0],select={'ekin':(0.511,None)})
+  #
+  #   returns the number of particles with :math:`ekin \in [0.511, +\infty] MeV` in function of x
+  #   wnorm=[1.0] to not divide nb of particles by bin width (otherwise number per um)
+  #
+  #   >>> hn(['r','ekin'],bwidth=[10.0,0.1],brange=[[0,1000],[0.1,50.0]],select={'x':150})
+  #
+  #   returns a number of e- per um per MeV at x=150 um
+  #   """
+  #   r=self._ps.raw
+  #
+  #   # Get a copy the axis from a str if needed
+  #   for i,ax in enumerate(axis):
+  #     if type(ax) is str:ax = eval("r.%s"%ax)
+  #     axis[i]=np.array(ax)
+  #
+  #   # Get a copy of particle statistical weight
+  #   w   = np.array(r.w)
+  #
+  #   # Filter the data if needed
+  #   if select is not None:
+  #     for faxis,frange in select.items():
+  #       w = r.select(w,faxis,frange) # FIXME: bug if more than 1 axis
+  #       for i,ax in enumerate(axis):
+  #         axis[i]=r.select(ax,faxis,frange)
+  #
+  #   # Define default bin range
+  #   if not brange: brange=[[None,None]]*len(axis)
+  #   for i,br in enumerate(brange):
+  #     if br[0] is None:brange[i][0]=min(axis[i])
+  #     if br[1] is None:brange[i][1]=max(axis[i])
+  #   """
+  #   # Define default bin width
+  #
+  #   # Calculate blen from bwidth
+  #
+  #   # Define default number of bins
+  #   if not blen: blen=[None]*len(axis)
+  #   for i,bl in enumerate(blen):
+  #     if bl is None: blen[i]=int(np.ceil((brange[i][1] + bwidth[i] - brange[i][0])/bwidth[i]))
+  #
+  #
+  #   bins.append(np.linspace(brange[i][0],brange[i][1]+bwidth[i],blen[i]) # FIXME: nope because of bwidth possibly not def.
+  #   """
+  #   # Calculate number of bins from bwidth if needed
+  #   if not bwidth: bwidth=[None]*len(axis)
+  #   for i,bw in enumerate(bwidth):
+  #     blen=brange[i][1] - brange[i][0]
+  #     if bw is None: bwidth[i]=blen/10.
+  #
+  #   # Define default bin width + number of bins
+  #   nbins=[]
+  #   if not bwidth: bwidth=[None]*len(axis)
+  #   for i,bw in enumerate(bwidth):
+  #     blen=brange[i][1] - brange[i][0]
+  #     if bw is None: bwidth[i]=blen/10.
+  #     nbins.append(int(np.ceil(np.nan_to_num(blen/bwidth[i])))) # TODO: delete nan_to_num?
+  #
+  #   # Construct the bins list
+  #   bins=[]
+  #   for i,ax in enumerate(axis):
+  #     if nbins[i]>0:
+  #       bins.append(np.linspace(brange[i][0],brange[i][1]+bwidth[i],nbins[i]+1))
+  #     else:
+  #       bins.append(1)
+  #
+  #     #bins.append(np.arange(brange[i][0],brange[i][1],bwidth[i]))
+  #
+  #   # Define weight normalization
+  #   if not wnorm: wnorm=[None]*len(axis)
+  #   for i,wn in enumerate(wnorm):
+  #     if wn is None: wnorm[i]=bwidth[i]
+  #
+  #   # Calculate the multi dimensional histo, normalized by wnorm
+  #   h,b=np.histogramdd(axis,weights=w/np.product(wnorm),bins=bins)
+  #
+  #   # Return the bins and histo
+  #   return b,h
+
   def hn(self,axis,blen=None,bwidth=None,brange=None,wnorm=None,select=None):
     """
     Create and return the n-dimensional histo of axis list.
-    
+
     Parameters
     ----------
     axis : list of str/np.array
@@ -27,107 +142,87 @@ class _Hist(object):
       weight normalization. If a wnorm element is None, the bin width is taken
     select : dict, optional
       filtering dictionary
-    
+
     Returns
     -------
     b : np.array
       bins
     h : np.array
       number of particles per bin unit
-    
+
     Notes
     -----
     TODO: If the given maximum bin range does not match with an int number of bins, the last bin is oversized ??
     it reduce bwidth to fit brange[1]-brange[0] with a int nb of bins
-    
+
     If blen and bwidth are both defined, priority is given to blen.
-    
+
     Examples
     --------
-    
+
     >>> hn(['x'],bwidth=[50],brange=[[0,1000]],wnorm=[1.0],select={'ekin':(0.511,None)})
-    
+
     returns the number of particles with :math:`ekin \in [0.511, +\infty] MeV` in function of x
     wnorm=[1.0] to not divide nb of particles by bin width (otherwise number per um)
-    
+
     >>> hn(['r','ekin'],bwidth=[10.0,0.1],brange=[[0,1000],[0.1,50.0]],select={'x':150})
-    
+
     returns a number of e- per um per MeV at x=150 um
     """
     r=self._ps.raw
-    
+
     # Get a copy the axis from a str if needed
     for i,ax in enumerate(axis):
       if type(ax) is str:ax = eval("r.%s"%ax)
       axis[i]=np.array(ax)
-      
-    # Get a copy of particle statistical weight 
+
+    # Get a copy of particle statistical weight
     w   = np.array(r.w)
-    
+
     # Filter the data if needed
     if select is not None:
       for faxis,frange in select.items():
         w = r.select(w,faxis,frange) # FIXME: bug if more than 1 axis
         for i,ax in enumerate(axis):
           axis[i]=r.select(ax,faxis,frange)
-    
+
     # Define default bin range
-    if not brange: brange=[[None,None]]*len(axis) 
-    for i,br in enumerate(brange):
-      if br[0] is None:brange[i][0]=min(axis[i])
-      if br[1] is None:brange[i][1]=max(axis[i])
-    """
-    # Define default bin width
-    
-    # Calculate blen from bwidth
-    
-    # Define default number of bins
-    if not blen: blen=[None]*len(axis)
-    for i,bl in enumerate(blen):
-      if bl is None: blen[i]=int(np.ceil((brange[i][1] + bwidth[i] - brange[i][0])/bwidth[i]))
-      
-    
-    bins.append(np.linspace(brange[i][0],brange[i][1]+bwidth[i],blen[i]) # FIXME: nope because of bwidth possibly not def.
-    """
-    # Calculate number of bins from bwidth if needed
-    if not bwidth: bwidth=[None]*len(axis)
-    for i,bw in enumerate(bwidth):
-      blen=brange[i][1] - brange[i][0]
-      if bw is None: bwidth[i]=blen/10.
-    
-    # Define default bin width + number of bins
-    nbins=[]
-    if not bwidth: bwidth=[None]*len(axis)
-    for i,bw in enumerate(bwidth):
-      blen=brange[i][1] - brange[i][0]
-      if bw is None: bwidth[i]=blen/10.
-      nbins.append(int(np.ceil(np.nan_to_num(blen/bwidth[i])))) # TODO: delete nan_to_num?
-    
-    # Construct the bins list
+    if wnorm is None    : wnorm=[None]*len(axis)
+    if brange is None   : brange=[[None,None]]*len(axis)
+    if bwidth is None   : bwidth=[None]*len(axis)
+    if blen is None     : blen=[None]*len(axis)
     bins=[]
-    for i,ax in enumerate(axis):
-      if nbins[i]>0:
-        bins.append(np.linspace(brange[i][0],brange[i][1]+bwidth[i],nbins[i]+1))
+    for i,_ in enumerate(axis):
+      if brange[i][0] is None:brange[i][0]=min(axis[i])
+      if brange[i][1] is None:brange[i][1]=max(axis[i])
+    #   if brange[i][0]==brange[i][1]:
+    #     brange[i][1]=2
+    #     blen[i]=2
+      if blen[i] is None:
+        if bwidth[i] is not None:
+          blen[i]   = int(np.ceil((brange[i][1] + bwidth[i] - brange[i][0])/bwidth[i]))
+        else:
+          blen[i]   = 10
+          bwidth[i] = (brange[i][1] - brange[i][0])/blen[i]
       else:
-        bins.append(1)
-      
-      #bins.append(np.arange(brange[i][0],brange[i][1],bwidth[i]))
-    
-    # Define weight normalization
-    if not wnorm: wnorm=[None]*len(axis)
-    for i,wn in enumerate(wnorm):
-      if wn is None: wnorm[i]=bwidth[i]
-    
+        bwidth[i] = (brange[i][1] - brange[i][0])/blen[i]
+      if wnorm[i] is None: wnorm[i]=bwidth[i]
+      bins.append(np.linspace(brange[i][0],brange[i][1]+bwidth[i],blen[i]))
+
+    print(bins)
+    print(brange)
+    print(bwidth)
+    print(blen)
     # Calculate the multi dimensional histo, normalized by wnorm
     h,b=np.histogramdd(axis,weights=w/np.product(wnorm),bins=bins)
-    
+
     # Return the bins and histo
     return b,h
-    
+
   def h1(self,axis,bwidth=None,brange=None,wnorm=None,select=None):
     """
     Create and return the 1 dimensional histogram of given axis.
-    
+
     Parameters
     ----------
     axis : str or np.array
@@ -138,36 +233,36 @@ class _Hist(object):
       bin maximum and minimum. If a brange element is None, the axis minimum/maximum is taken
     select : dict, optional
       filtering dictionnary
-    
+
     Returns
     -------
     b : np.array
       bins
     h : np.array
       histogram
-      
+
     Notes
     -----
     the h1 method is just a different way to call the generic method hn
-    
+
     See Also
     --------
     hn, h2, h3
     """
     if not brange : brange = [None,None]
-    
+
     b,h=self.hn([axis],bwidth=[bwidth],brange=[brange],wnorm=[wnorm],select=select)
-    
-    # Verifier b[:-1]
-    h=list(h)
-    h.append(0.0)
-    h=np.array(h)
+
+    # # Verifier b[:-1]
+    # h=list(h)
+    # h.append(0.0)
+    # h=np.array(h)
     return b[0],h
-    
+
   def h2(self,axis1,axis2,bwidth1=None,bwidth2=None,brange1=None,brange2=None,wnorm=None,select=None):
     """
     Create and return the 2 dimensional histogram of given axis.
-    
+
     Parameters
     ----------
     axis1, axis2 : str or np.array
@@ -178,33 +273,33 @@ class _Hist(object):
       bin maximum and minimum. If a brange element is None, the axis minimum/maximum is taken
     select : dict, optional
       filtering dictionnary
-    
+
     Returns
     -------
     b1, b2 : np.array
       bins
     h : np.array
       histogram
-      
+
     Notes
     -----
     the h2 method is just a different way to call the generic method hn
-    
+
     See Also
     --------
     hn, h1, h3
     """
     if not brange1 : brange1 = [None,None]
     if not brange2 : brange2 = [None,None]
-    
+
     b,h=self.hn([axis1,axis2],bwidth=[bwidth1,bwidth2],brange=[brange1,brange2],wnorm=wnorm,select=select)
-    
+
     return b[0],b[1],h
-    
+
   def h3(self,axis1,axis2,axis3,bwidth1=None,bwidth2=None,bwidth3=None,brange1=None,brange2=None,brange3=None,select=None):
     """
     Create and return the 3 dimensional histogram of given axis.
-    
+
     Parameters
     ----------
     axis1, axis2, axis3 : str or np.array
@@ -215,18 +310,18 @@ class _Hist(object):
       bin maximum and minimum. If a brange element is None, the axis minimum/maximum is taken
     select : dict, optional
       filtering dictionnary
-    
+
     Returns
     -------
     b1, b2, b3 : np.array
       bins
     h : np.array
       histogram
-      
+
     Notes
     -----
     the h3 method is just a different way to call the generic method hn
-    
+
     See Also
     --------
     hn, h1, h2
@@ -236,6 +331,5 @@ class _Hist(object):
     if not brange3 : brange3 = [None,None]
 
     b,h=self.hn([axis1,axis2,axis3],bwidth=[bwidth1,bwidth2,bwidth3],brange=[brange1,brange2,brange3],select=select)
-    
-    return b[0],b[1],b[2],h
 
+    return b[0],b[1],b[2],h
--- p2sat/_Tools.py
+++ p2sat/_Tools.py
@@ -4,23 +4,23 @@ import numpy as np
 class _Tools(object):
   """
   Tools
-  
+
   Containing various tools, such as IO into a p2sat file or fits
   """
   def __init__(self,PhaseSpace):
     self._ps=PhaseSpace
-    
+
   def import_data(self,file_name,verbose=True):
     """
     Import particle phase space from a p2sat file.
-    
+
     Parameters
     ----------
     file_name : str
       name of the input file
     verbose : bool, optional
       verbosity of the function. If True, a message is displayed when the data is imported
-    
+
     See Also
     --------
     export_data
@@ -39,11 +39,11 @@ class _Tools(object):
     self._ps.raw.update(w,x,y,z,px,py,pz,verbose=verbose)
     if verbose: print('Data succesfully imported')
 
-  
+
   def export_data(self,file_name,title="",verbose=True):
     """
     Export particle phase space in a p2sat file.
-    
+
     Parameters
     ----------
     file_name : str
@@ -52,7 +52,7 @@ class _Tools(object):
       title of the file
     verbose : bool, optional
       verbosity of the function. If True, a message is displayed when the data is exported
-    
+
     Notes
     -----
     The format in the output file is
@@ -64,60 +64,60 @@ class _Tools(object):
       . . . . .  .  .
       . . . . .  .  .
     with 7 digits precision in scientific notation
-    
+
     Some text can be written if the first character of the line is a "#".
-    
+
     See Also
     --------
     import_data
-    
+
     TODO: add parameter 'header=True' to use header or not ?
     """
     if verbose: print("Exporting data ...")
-    
+
     r=self._ps.raw
-    
+
     # Opening the output file
     with open(file_name,'w') as f:
-      
+
       # Write title
       f.write("# Title : %s\n"%title)
-      
+
       # Write legend
       f.write("# ")
       for legend in ('weight','x (um)','y (um)','z (um)','px (MeV/c)','py (MeV/c)','pz (MeV/c)'):
         f.write("%-16s"%legend) # the chain is placed under 16 characters
       f.write("\n")
-      
+
       # Write data
       for i in range(len(r.w)):
         for e in [r.w[i],r.x[i],r.y[i],r.z[i],r.px[i],r.py[i],r.pz[i]]:
             tmp="% .7E"%e # 7 digits precision with E notation
             f.write("%-16s"%tmp) # the chain is placed under 16 characters
         f.write("\n")
-    
+
     if verbose: print('Data succesfully exported')
-  
-  def discretize(self,update=False,verbose=True,**kargs):
+
+  def discretize_1(self,update=False,verbose=True,**kargs):
     """
     Discretize the particles phase space into a given number of bins.
-    
+
     Parameters
     ----------
     ...
-    
-    
+
+
     Notes
     -----
     ...
-    
+
     Examples
     --------
     ...
     """
     hn=self._ps.hist.hn
     r=self._ps.raw
-    
+
     """
     if not nbins:nbins=[10]*6
     brange=[[min(r.x),max(r.x)],
@@ -138,7 +138,7 @@ class _Tools(object):
     bx,by,bz,bpx,bpy,bpz=bi
     """
     hi,b=np.histogramdd([r.x,r.y,r.z,r.px,r.py,r.pz],weights=r.w,bins=nbins)
-    
+
     bx=b[0]
     by=b[1]
     bz=b[2]
@@ -150,7 +150,7 @@ class _Tools(object):
     w       = []
     x,y,z   = [],[],[]
     px,py,pz= [],[],[]
-    
+
     if verbose : print('Looping over all the configurations ...')
     for ix,ex in enumerate(hi):
       if verbose: print('Step %s of %s ...'%(ix+1,len(hi)))
@@ -167,28 +167,88 @@ class _Tools(object):
                   px.append(bpx[ipx])
                   py.append(bpy[ipy])
                   pz.append(bpz[ipz])
-    
+
     if verbose : print('Done !')
-    
+
     if update:
       r.update(w,x,y,z,px,py,pz,verbose)
     else:
       return np.array(w),np.array(x),np.array(y),np.array(z),np.array(px),np.array(py),np.array(pz)
-    
-    
-  def rotate(self,angle):
+
+  def discretize_2(self,update=False,verbose=True,**kargs):
     """
-    Rotate the particles phase space of a given angle.
-    
+    Discretize the particles phase space in a 6 or 7 D histogram.
+
+    Parameters
+    ----------
+    update : bool, optional
+      True for updating raw data with new discretized values. Default is False
+    verbose : bool, optional
+      verbosity. Default is True
+    kargs
+      optional keyword arguments to pass to the hist.hn function
+
+    Notes
+    -----
+    This method can be used to significantly reduce disk space usage
+    when saving data into output file.
+
+    Examples
+    --------
+    ...
+
+    See Also
+    --------
+    hist.hn
+    """
+    hn=self._ps.hist.hn
+    r=self._ps.raw
+    print(r.w)
+    if verbose : print('Data discretization ...')
+    #bi,hi=hn(['x','y','z','px','py','pz'],bwidth=bwidth,brange=brange,wnorm=[1.0]*6,select=select)
+    bi,hi=hn([r.x,r.y,r.z,r.px,r.py,r.pz],wnorm=[1.0]*6,**kargs)
+    bx,by,bz,bpx,bpy,bpz=bi
+    print(hi)
+    if verbose : print('Done !')
+    w       = []
+    x,y,z   = [],[],[]
+    px,py,pz= [],[],[]
+
+    if verbose : print('Getting new configurations ...')
+    hb=hi.nonzero()
+    print(hb)
+
+    w   = hi[hb]
+    x   = bx[hb[0]]
+    y   = by[hb[1]]
+    z   = bz[hb[2]]
+    px  = bpx[hb[3]]
+    py  = bpy[hb[4]]
+    pz  = bpz[hb[5]]
+
+    if verbose : print('Done !')
+
+    if update:
+      r.update(w,x,y,z,px,py,pz,verbose)
+    else:
+      return np.array(w),np.array(x),np.array(y),np.array(z),np.array(px),np.array(py),np.array(pz)
+
+  def discretize_MT(self):
+    pass
+
+  def transformate(self,translation=(0.0,0.0,0.0),rotation=(0.0,0.0)):
+    """
+    Transformate the particle phase space with given translation and rotation.
+
     Parameters
     ----------
     ...
-    
-    
+
+
     Notes
     -----
     ...
-    
+
     Examples
     --------
     ...
